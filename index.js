fb3605b5454b39a0877df37b83a15896abbec0b2
Bertrand-dolmen <servicefinancie0@gmail.com>
2025-04-06 10:24:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-import React from "react";
-
 export default function Home() {
   return (
-    <div className="p-6">
-      <h1 className="text-4xl font-bold mb-4">Prêt d'argent</h1>
-      <p>Bienvenue sur notre plateforme de prêt d'argent entre particuliers.</p>
+    <div>
+      <h1>Bienvenue sur Prêt d'argent</h1>
+      <p>Plateforme d’annonces de prêt d’argent entre particuliers</p>
     </div>
   );
 }
