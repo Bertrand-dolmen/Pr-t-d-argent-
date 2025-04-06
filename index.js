0cb150e11c35dd423c6a40be8ef62bd17cd355ec
Bertrand-dolmen <servicefinancie0@gmail.com>
2025-04-06 10:09:20 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-4">Prêt d'argent</h1>
+      <p>Bienvenue sur notre plateforme de prêt d'argent entre particuliers.</p>
+    </div>
+  );
+}
